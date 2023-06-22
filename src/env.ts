38f611a1e432d6b2eac30fe2fefaa517a8fbf859
enisdenjo <badurinadenis@gmail.com>
2023-06-22 15:56:38 +0200

    debug env

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,19 @@ import { z } from 'zod';
 
 export const env = z
   .object({
+    DEBUG: z
+      .string()
+      .default('0')
+      .transform((arg) => ['1', 'y', 'yes', 't', 'true'].includes(arg)),
     CLICKHOUSE_KEY_ID: z.string(),
     CLICKHOUSE_KEY_SECRET: z.string(),
     CLICKHOUSE_ORGANIZATION_ID: z.string(),
     CLICKHOUSE_INSTANCE_ID: z.string(),
   })
   .parse(process.env);
+
+if (!env.DEBUG) {
+  console.debug = () => {
+    // noop
+  };
+}
