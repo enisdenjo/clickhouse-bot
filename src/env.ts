a62f26b75e1817f80f1ad3a0b43a498292e36258
enisdenjo <badurinadenis@gmail.com>
2023-06-23 18:18:31 +0200

    have protected instance id appear

--- src/env.ts
+++ src/env.ts
@@ -8,7 +8,10 @@ export const env = z
       .transform((arg) => ['1', 'y', 'yes', 't', 'true'].includes(arg)),
     CLICKHOUSE_KEY_ID: z.string(),
     CLICKHOUSE_KEY_SECRET: z.string(),
-    CLICKHOUSE_PROTECTED_INSTANCE_ID: z.string().optional(),
+    CLICKHOUSE_PROTECTED_INSTANCE_ID: z
+      .string()
+      // so that the env var appears in the help command
+      .default(''),
   })
   .parse(process.env);
 
